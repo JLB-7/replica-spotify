3f4e15b6a74a085ea8b77d3c8b1153ee8f7dbc34
JLB-7 <luisbarauna25@gmail.com>
2025-03-20 12:00:27 -0300

    ajuste no process env

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -11,22 +11,22 @@ app.use(cors());
 
 app.get("api/", (request, response) => {
     response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'.");
-})
+});
 
 app.get("api/artists", async (request, response) => {
     response.send(await db.collection("artists").find({}).toArray());
-})
+});
 
 app.get("api/songs", async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray());
-})
+});
 
 app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
 app.get("*", async (request, response) => {
     response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
-})
+});
 
 app.listen(PORT, () => {
-    console.log("O servidor está escutando na porta " + PORT)
-})
+    console.log(`O servidor está escutando na porta ${PORT}`);
+});
